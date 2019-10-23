28f20c7531b4a05385a5f5b763cf4e1509374900
Christopher Mao <chrismao2006@gmail.com>
2019-10-23 14:52:07 -0500

    Change to react charts

--- src/Chart.js
+++ src/Chart.js
@@ -12,49 +12,80 @@ export default class Chart extends React.Component {
 
     var csv = require(`./data/${this.props["csvName"]}`);
 
-    this.state = {"data": []}
+    this.state = {"x": [], "y_true": [], "y_pred": []}
+    let x = [], y_true = [], y_pred = [];
     d3.csv(csv).then(data => {
-      console.log(data)
-      const y_true = data.map(cell => {
-        let ret = {}
-        // ret[cell["index"]] = Math.round(parseFloat(cell["y_true"])/5000)
-        // ret[cell["index"]] = 5
-        ret["x"] = Math.round(parseFloat(cell["index"]))
-        ret["y_true"] = parseFloat(cell["y_true"])
-        ret["y_pred"] = parseFloat(cell["y_pred"])
-        return(ret)
+      data.forEach(cell => {
+        x.push(Math.round(parseFloat(cell["index"])))
+        y_true.push(parseFloat(cell["y_true"]))
+        y_pred.push(parseFloat(cell["y_pred"]))
+        
       })
+      console.log(x)
       console.log(y_true)
-      this.setState({"y_true": y_true})
-      // this.state = {"y_true": y_true}
+      console.log(y_pred)
+      this.setState({"x": x, "y_true": y_true, "y_pred": y_pred})
+      //   ...this.state, "x": [...this.state.x,Math.round(parseFloat(cell["index"]))]})
+      // this.setState({...this.state, "y_true": [...this.state.y_true,parseFloat(cell["y_true"])]})
+      // this.setState({...this.state, "y_pred": [...this.state.y_pred,parseFloat(cell["y_pred"])]})
     });
   }
 
   render() {
     return (
       <React.Fragment>
-        <Title>{this.props["title"]}</Title>
+        <Title>{this.props["title"]}</Title>yar
         <ResponsiveContainer>
-          {/* <ChartJSLine data={this.state["y_true"]}/> */}
-          <LineChart
-          data={this.state["y_true"]}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis dataKey="x" />
-          <YAxis>
-            <Label angle={270} position="left" style={{ textAnchor: 'middle' }}>
-              Energy Prediction (W)
-            </Label>
-          </YAxis>
-          <Line type="monotone" dataKey="y_true" stroke="#556CD6" dot={false} />
-          <Line type="monotone" dataKey="y_pred" stroke="#ff0000" dot={false} />
-        </LineChart>
+          <ChartJSLine data= {{
+                labels: this.state.x,
+                datasets: [
+                    {
+                        label: 'Energy Consumption Actual',
+                        data: this.state.y_true,
+                        fill: false,
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1,
+                        pointRadius: 0
+                    },
+                    {
+                        label: 'Energy Consumption Prediction',
+                        data: this.state.y_pred,
+                        fill: false,
+                        backgroundColor: 'rgba(99, 132, 255, 0.2)',
+                        borderColor: 'rgba(99, 132, 255, 1)',
+                        borderWidth: 1,
+                        pointRadius: 0
+                    }                
+                ]
+            }} options= {{
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:true
+                      }
+                  }]
+              },
+              pan: {
+                  // Boolean to enable panning
+                  enabled: true,
 
+                  // Panning directions. Remove the appropriate direction to disable 
+                  // Eg. 'y' would only allow panning in the y direction
+                  mode: 'xy'
+              },
+
+              // Container for zoom options
+              zoom: {
+                  // Boolean to enable zooming
+                  enabled: true,
+                  drag: true,
+                  // Zooming directions. Remove the appropriate direction to disable 
+                  // Eg. 'y' would only allow zooming in the y direction
+                  mode: 'xy',
+              }
+          }}            
+          />
         </ResponsiveContainer>
       </React.Fragment>
     );
